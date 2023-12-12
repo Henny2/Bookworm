b8e96d102ffe8be9ebacf511b33619481ef20599
Henny2 <henrieke.baunack@web.de>
2023-12-11 20:06:00 -0800

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Henrieke Baunack on 12/11/23.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
