569beba26adb00d72eb2c3b961b1198c65779064
Henny2 <henrieke.baunack@web.de>
2023-12-15 20:49:28 -0800

    adding delete functionality

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -10,7 +10,11 @@ import SwiftUI
 struct ContentView: View {
     @Environment(\.modelContext) var modelContext
 
-    @Query var books: [Book]
+    @Query(sort: [
+        // sorts by title first, then by author
+        SortDescriptor(\Book.title),
+        SortDescriptor(\Book.author)
+    ]) var books: [Book]
     
     @State private var showingAddScreen = false
     var body: some View {
@@ -29,9 +33,16 @@ struct ContentView: View {
                         }
                     }
                 }
+                .onDelete(perform: deleteBooks)
             }
             .navigationTitle("Bookworm")
+            .navigationDestination(for: Book.self, destination: { book in
+                DetailView(book: book)
+            })
             .toolbar {
+                ToolbarItem(placement: .topBarLeading){
+                    EditButton()
+                }
                 ToolbarItem(placement: .topBarTrailing) {
                     Button("Add Book", systemImage :"plus") {
                         showingAddScreen.toggle()
@@ -40,6 +51,13 @@ struct ContentView: View {
             }.sheet(isPresented: $showingAddScreen) {
                 AddBookView()
             }
+        }
+    }
+    
+    func deleteBooks(at offsets: IndexSet){
+        for offset in offsets {
+            let book = books[offset] // go over all the things you were asked to delete
+            modelContext.delete(book) // find the book and delete it
         }
     }
 }
--- Bookworm/DetailView.swift
+++ Bookworm/DetailView.swift
@@ -5,14 +5,53 @@
 //  Created by Henrieke Baunack on 12/14/23.
 //
 
+import SwiftData
 import SwiftUI
 
 struct DetailView: View {
+    
+    let book: Book
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            ZStack(alignment: .bottomTrailing) {
+                Image(book.genre)
+                    .resizable()
+                    .scaledToFit()
+                
+                Text(book.genre.uppercased())
+                    .fontWeight(.black)
+                    .padding(8)
+                    .foregroundStyle(.white)
+                    .background(.black.opacity(0.75))
+                    .clipShape(.capsule)
+                    .offset(x: -5, y: -5)
+            }
+            
+            Text(book.author)
+                .font(.title)
+                .foregroundStyle(.secondary)
+            
+            Text(book.review)
+                .padding()
+            
+            RatingView(rating: .constant(book.rating))
+                .font(.largeTitle)
+        }
+        .navigationTitle(book.title)
+        .navigationBarTitleDisplayMode(.inline)
+        .scrollBounceBehavior(.basedOnSize)
     }
 }
 
 #Preview {
-    DetailView()
+    do {
+        let config = ModelConfiguration(isStoredInMemoryOnly: true)
+        let container = try ModelContainer(for: Book.self, configurations: config)
+        let example = Book(title: "Test Book", author: "J K Rowling", genre: "Fantasy", review: "This was so much fun to read! Can highly recommend.", rating: 4)
+        return DetailView(book: example)
+            .modelContainer(container)
+    } catch {
+        return Text("Failed to create preview: \(error.localizedDescription)")
+    }
 }
